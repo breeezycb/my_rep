e3e553593387a2da7913dafdb6f358b069cfa373
breeezycb <sanchezkru@yandex.ru>
2025-04-08 16:22:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import utils
 print (utils.add(10, 20)) # сумма = 30
+
+from math_operations import addition
+print (addition.add(20,20))
+
+from math_operations import subtraction
+print (subtraction.sub(30,20))
